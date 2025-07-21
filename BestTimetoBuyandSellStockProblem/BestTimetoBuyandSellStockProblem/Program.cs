b0ecf233def4aee6f35692fbdf3c11fee2c46c5b
sujon.sarder <sujon.sarder@IT-SUJON>
2025-07-21 13:17:27 +0600

    Best Time to Buy and Sell Stock problem solved

---
+++ BestTimetoBuyandSellStockProblem/BestTimetoBuyandSellStockProblem/Program.cs
@@ -0,0 +1,31 @@
+﻿public class Solution
+{
+    public int MaxProfit(int[] prices)
+    {
+        if (prices == null || prices.Length == 0) return 0;
+
+        int minPrice = prices[0];
+        int maxProfit = 0;
+
+        for (int i = 1; i < prices.Length; i++)
+        {
+
+            if (prices[i] < minPrice)
+            {
+                minPrice = prices[i];
+            }
+            else
+            {
+
+                int profit = prices[i] - minPrice;
+                if (profit > maxProfit)
+                {
+                    maxProfit = profit;
+                }
+            }
+        }
+
+        return maxProfit;
+    }
+}
+
